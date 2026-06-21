cc215f8497dc3de4d5dc421c4bbe7d883840609f
Greedrache <greedrache@gmail.com>
2026-06-21 09:57:31 +0200

    Hero + Header hinzugefügt

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrls: ['./navbar.scss'],
 })
 export class Navbar {}
