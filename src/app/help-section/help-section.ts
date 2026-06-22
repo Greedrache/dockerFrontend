b2369107f99fc28781eecebfc9c69befb5f52376
Greedrache <greedrache@gmail.com>
2026-06-22 11:08:55 +0200

    Footer, About, Help sections hinzugefügt

---
+++ src/app/help-section/help-section.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './help-section.html',
+  styleUrls: ['./help-section.scss'],
+})
+export class HelpSection {}
